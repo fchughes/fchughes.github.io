1b48e4d2df38c21054b2a26dfd12bd53a411d5b0
fchughes <forestchughes@gmail.com>
2023-07-13 16:23:13 -0700

    retrigger gh-pages action

--- svelte.config.js
+++ svelte.config.js
@@ -32,7 +32,7 @@ const config = {
   kit: {
     adapter: adapter(),
 	paths: {
-		base: process.env.NODE_ENV === 'production' ? '/fchughes.github.io' : '/',
+		base: process.env.NODE_ENV === 'production' ? '/fchughes.github.io' : '',
 	}
   },
 };
