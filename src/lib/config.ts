2fd80cc28169520a3cf77217217667e1c4398571
fchughes <forestchughes@gmail.com>
2023-07-13 15:44:15 -0700

    initial commit

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'fchughes.com'
+export const description = 'Forest C. Hughes portfolio and blog'
+export const url = dev ? 'http://localhost:5173' : 'https://fchughes.github.io'
